a72a2452696e231018e07ad797b5a9712a2e3378
BuckarooBanzay <BuckarooBanzay@github.com>
2019-10-04 13:43:38 +0200

    skybox import

---
+++ init.lua
@@ -0,0 +1,56 @@
+
+epic.register_skybox("Mars", {
+  "mars_up.jpg^[transformR270",
+  "mars_dn.jpg^[transformR90",
+  "mars_ft.jpg",
+  "mars_bk.jpg",
+  "mars_lf.jpg",
+  "mars_rt.jpg"
+})
+
+epic.register_skybox("Space with Earth", {
+  "space_sky.png",
+  "space_sky2.png",
+  "space_sky.png",
+  "space_sky.png",
+  "space_sky.png",
+  "space_sky.png"
+})
+
+epic.register_skybox("Moon", {
+  "space_sky.png",
+  "space_sky.png",
+  "space_sky.png",
+  "space_sky.png",
+  "space_sky.png",
+  "space_sky.png"
+})
+
+epic.register_skybox("Deepspace", {
+  "sky_pos_z.png",
+  "sky_neg_z.png^[transformR180",
+  "sky_neg_y.png^[transformR270",
+  "sky_pos_y.png^[transformR270",
+  "sky_pos_x.png^[transformR270",
+  "sky_neg_x.png^[transformR90"
+})
+
+epic.register_skybox("Plain Black", {
+	color = {r=0, g=0, b=0}
+})
+
+epic.register_skybox("Plain Red", {
+	color = {r=255, g=0, b=0}
+})
+
+epic.register_skybox("Plain Green", {
+	color = {r=0, g=255, b=0}
+})
+
+epic.register_skybox("Plain Blue", {
+	color = {r=0, g=0, b=255}
+})
+
+epic.register_skybox("Plain White", {
+	color = {r=255, g=255, b=255}
+})
