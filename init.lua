bd98cdaced18a77f70f4e3ec90f61e470380e4fd
Thomas Rudin <thomas@rudin.io>
2019-10-05 13:31:21 +0200

    skybox fixes

--- init.lua
+++ init.lua
@@ -1,56 +1,73 @@
 
-epic.register_skybox("Mars", {
-  "mars_up.jpg^[transformR270",
-  "mars_dn.jpg^[transformR90",
-  "mars_ft.jpg",
-  "mars_bk.jpg",
-  "mars_lf.jpg",
-  "mars_rt.jpg"
+epic.register_skybox({
+  name = "Mars",
+  textures = {
+    "mars_up.jpg^[transformR270",
+    "mars_dn.jpg^[transformR90",
+    "mars_ft.jpg",
+    "mars_bk.jpg",
+    "mars_lf.jpg",
+    "mars_rt.jpg"
+  }
 })
 
-epic.register_skybox("Space with Earth", {
-  "space_sky.png",
-  "space_sky2.png",
-  "space_sky.png",
-  "space_sky.png",
-  "space_sky.png",
-  "space_sky.png"
+epic.register_skybox({
+  name = "Space with Earth",
+  textures = {
+    "space_sky.png",
+    "space_sky2.png",
+    "space_sky.png",
+    "space_sky.png",
+    "space_sky.png",
+    "space_sky.png"
+  }
 })
 
-epic.register_skybox("Moon", {
-  "space_sky.png",
-  "space_sky.png",
-  "space_sky.png",
-  "space_sky.png",
-  "space_sky.png",
-  "space_sky.png"
+epic.register_skybox({
+  name = "Moon",
+  textures = {
+    "space_sky.png",
+    "space_sky.png",
+    "space_sky.png",
+    "space_sky.png",
+    "space_sky.png",
+    "space_sky.png"
+  }
 })
 
-epic.register_skybox("Deepspace", {
-  "sky_pos_z.png",
-  "sky_neg_z.png^[transformR180",
-  "sky_neg_y.png^[transformR270",
-  "sky_pos_y.png^[transformR270",
-  "sky_pos_x.png^[transformR270",
-  "sky_neg_x.png^[transformR90"
+epic.register_skybox({
+  name = "Deepspace",
+  textures = {
+    "sky_pos_z.png",
+    "sky_neg_z.png^[transformR180",
+    "sky_neg_y.png^[transformR270",
+    "sky_pos_y.png^[transformR270",
+    "sky_pos_x.png^[transformR270",
+    "sky_neg_x.png^[transformR90"
+  }
 })
 
-epic.register_skybox("Plain Black", {
+epic.register_skybox({
+  name = "Plain Black",
 	color = {r=0, g=0, b=0}
 })
 
-epic.register_skybox("Plain Red", {
+epic.register_skybox({
+  name = "Plain Red",
 	color = {r=255, g=0, b=0}
 })
 
-epic.register_skybox("Plain Green", {
+epic.register_skybox({
+  name = "Plain Green",
 	color = {r=0, g=255, b=0}
 })
 
-epic.register_skybox("Plain Blue", {
+epic.register_skybox({
+  name = "Plain Blue",
 	color = {r=0, g=0, b=255}
 })
 
-epic.register_skybox("Plain White", {
+epic.register_skybox({
+  name = "Plain White",
 	color = {r=255, g=255, b=255}
 })
