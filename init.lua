7c6533b167e94b31a40c3f6ab17037d7bd706ecf
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-05-25 12:52:48 +0200

    swap registration dofile's

--- init.lua
+++ init.lua
@@ -7,8 +7,8 @@ epic_skybox = {
 local MP = minetest.get_modpath("epic_skybox")
 
 dofile(MP.."/alias.lua")
+dofile(MP.."/register.lua")
 dofile(MP.."/skyboxes.lua")
 dofile(MP.."/functions.lua")
-dofile(MP.."/register.lua")
 dofile(MP.."/chatcommands.lua")
 dofile(MP.."/epic_block.lua")
